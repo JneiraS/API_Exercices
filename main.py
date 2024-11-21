fea77f32551736f3a170df79ebd9b32ace944585
JneiraS <johnny.neira@gmail.com>
2024-11-21 20:16:23 +0000

    docs: Ajout d'une docstring.

--- main.py
+++ main.py
@@ -3,9 +3,13 @@ from exercice_two.api_client import display_temperature_forecasts
 
 
 def main():
+    """
+    Affiche les prévisions de température pour les 5 prochains jours,
+    les coordonnées de l'ISS, ainsi que les astronautes sur l'ISS.
+    """
     display_temperature_forecasts()
     display_iss_informations()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
