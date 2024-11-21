b2853ce0f07695850824b0b650c531ca28e3c000
NeiraJ <johnny.neira@gmail.com>
2024-11-21 09:20:05 +0100

    feat: Ajout de la classe IssAstronautsAPIClient et mise à jour du docstring de fetch_data de IssLocationAPIClient

--- exercice_one/api_client.py
+++ exercice_one/api_client.py
@@ -12,7 +12,6 @@ class IssLocationAPIClient(cf.APIClient):
     def fetch_data(self) -> dict:
         """
         Renvoie les données de la réponse de l'API Open Notify.
-        Renvoie un dictionnaire JSON, ou None en cas d'erreur.
         """
         try:
             response = requests.get(self.url_location_api)
@@ -20,6 +19,29 @@ class IssLocationAPIClient(cf.APIClient):
             return response.json()
         except requests.exceptions.RequestException as e:
             raise Exception(f"Erreur lors de la requ te API Open Notify: {e}") from e
+
+
+class IssAstronautsAPIClient(cf.APIClient):
+    def __init__(self):
+        self.url_astronauts_api = 'http://api.open-notify.org/astros.json'
+
+    def fetch_data(self) -> dict:
+        try:
+            response = requests.get(self.url_astronauts_api)
+            response.raise_for_status()
+            return response.json()
+        except requests.exceptions.RequestException as e:
+            raise Exception(f"Erreur lors de la requête API Open Notify: {e}") from e
+
+    def __str__(self):
+        """Renvoie une chaîne de caractères contenant les noms des astronautes."""
+        astronauts: list = self.fetch_data().get('people', [])
+
+        result = f"Il y actuellement {len(astronauts)} astronautes sur l'ISS:\n"
+        for personne in astronauts:
+            if personne['craft'] == 'ISS':
+                result += f"\t{personne['name']}\n"
+        return result
 
 
 def timestamp_to_date(timestamp: int) -> str:
