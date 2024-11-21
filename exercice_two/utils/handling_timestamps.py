e0d927c4b08aeeae56bd65f2746f97771e0074c2
NeiraJ <johnny.neira@gmail.com>
2024-11-21 11:16:33 +0100

    feat: Implémentation de la fonction timestamp_range_for_the_next_fives_days pour générer des plages de timestamps sur 5 jours

--- exercice_two/utils/handling_timestamps.py
+++ exercice_two/utils/handling_timestamps.py
@@ -3,9 +3,9 @@ from datetime import datetime, timedelta
 SECONDS_IN_A_DAY = 86400
 
 
-def find_tomorrow_timestamp_range():
+def find_tomorrow_timestamp_range() -> list[int]:
     """
-    Renvoie un tuple de deux timestamps, qui représentent respectivement le début et la fin du lendemain.
+    Renvoie une liste de deux timestamps, qui représentent respectivement le début et la fin du lendemain.
 
     La méthode fonctionne en ajoutant un jour à la date actuelle, puis en créant deux objets datetime :
     - le premier avec l'heure 00h00min00s,
@@ -19,3 +19,21 @@ def find_tomorrow_timestamp_range():
     tomorrow_end_timestamp = int(tomorrow_end.timestamp())
     tomorrow_timestamp_range = [tomorrow_start_timestamp, tomorrow_end_timestamp]
     return tomorrow_timestamp_range
+
+
+def timestamp_range_for_the_next_fives_days(start_range=None) -> list[list[int]]:
+    """
+    Renvoie une liste de 5 listes de timestamps, qui représentent respectivement le début et la fin de 5 jours consécutifs,
+    en partant de la plage de timestamps fournie en argument.
+
+    Chaque élément de la liste renvoyée contient deux timestamps, qui représentent le début et la fin d'un jour.
+    La méthode fonctionne en ajoutant 24h à chaque itération, en partant du premier timestamp de la liste d'origine.
+    """
+    if start_range is None:
+        start_range = find_tomorrow_timestamp_range()
+    increase_by = 0
+    result = []
+    for _ in range(5):
+        result.append(list(map(lambda x: x + increase_by, start_range)))
+        increase_by += SECONDS_IN_A_DAY
+    return result
