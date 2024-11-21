ae5d7612cc5a4cc8e5ad5b6a7a6560b65ace9cf0
NeiraJ <johnny.neira@gmail.com>
2024-11-21 16:13:49 +0100

    Améliorer la gestion des timestamps pour demain
    - Simplifier la fonction find_tomorrow_timestamp_range()
    - Utiliser datetime.replace() pour définir les heures exactes

--- exercice_two/utils/handling_timestamps.py
+++ exercice_two/utils/handling_timestamps.py
@@ -4,21 +4,13 @@ SECONDS_IN_A_DAY = 86400
 
 
 def find_tomorrow_timestamp_range() -> list[int]:
-    """
-    Renvoie une liste de deux timestamps, qui représentent respectivement le début et la fin du lendemain.
+    """Renvoie une liste de deux timestamps, qui représentent respectivement
+     le début et la fin du lendemain."""
 
-    La méthode fonctionne en ajoutant un jour à la date actuelle, puis en créant deux objets datetime :
-    - le premier avec l'heure 00h00min00s,
-    - le second avec l'heure 23h59min59s.
-    Enfin, la méthode renvoie les timestamps correspondants à ces deux objets datetime.
-    """
     tomorrow = datetime.today() + timedelta(days=1)
     tomorrow_start = tomorrow.replace(hour=0, minute=0, second=0)
     tomorrow_end = tomorrow.replace(hour=23, minute=59, second=59)
-    tomorrow_start_timestamp = int(tomorrow_start.timestamp())
-    tomorrow_end_timestamp = int(tomorrow_end.timestamp())
-    tomorrow_timestamp_range = [tomorrow_start_timestamp, tomorrow_end_timestamp]
-    return tomorrow_timestamp_range
+    return [int(tomorrow_start.timestamp()), int(tomorrow_end.timestamp())]
 
 
 def timestamp_range_for_the_next_fives_days(start_range=None) -> list[list[int]]:
@@ -34,6 +26,6 @@ def timestamp_range_for_the_next_fives_days(start_range=None) -> list[list[int]]
     increase_by = 0
     result = []
     for _ in range(5):
-        result.append(list(map(lambda x: x + increase_by, start_range)))
+        result.append(list(map(lambda x, inc=increase_by: x + inc, start_range)))
         increase_by += SECONDS_IN_A_DAY
     return result
