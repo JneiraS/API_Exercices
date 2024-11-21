42635709e00a2cc1b17c21c25c85ea2a6af107f3
JneiraS <johnny.neira@gmail.com>
2024-11-21 20:03:45 +0000

    refactor: Amélioration de la fonction timestamp_range_for_the_next_fives_days
    - Simplification du code en utilisant une compréhension de liste
    - Optimisation de la logique pour générer les plages horaires
    - Correction de la documentation pour refléter les changements

--- exercice_two/utils/handling_timestamps.py
+++ exercice_two/utils/handling_timestamps.py
@@ -5,7 +5,7 @@ SECONDS_IN_A_DAY = 86400
 
 def find_tomorrow_timestamp_range() -> list[int]:
     """Renvoie une liste de deux timestamps, qui représentent respectivement
-     le début et la fin du lendemain."""
+    le début et la fin du lendemain."""
 
     tomorrow = datetime.today() + timedelta(days=1)
     tomorrow_start = tomorrow.replace(hour=0, minute=0, second=0)
@@ -14,18 +14,13 @@ def find_tomorrow_timestamp_range() -> list[int]:
 
 
 def timestamp_range_for_the_next_fives_days(start_range=None) -> list[list[int]]:
-    """
-    Renvoie une liste de 5 listes de timestamps, qui représentent respectivement le début et la fin de 5 jours consécutifs,
-    en partant de la plage de timestamps fournie en argument.
-
-    Chaque élément de la liste renvoyée contient deux timestamps, qui représentent le début et la fin d'un jour.
-    La méthode fonctionne en ajoutant 24h à chaque itération, en partant du premier timestamp de la liste d'origine.
+    """Renvoie une liste de 5 plages de timestamps, qui représentent respectivement
+    les 5 prochains jours. Chaque plage est une liste de deux timestamps,
+    qui représentent respectivement le début et la fin du jour.
     """
     if start_range is None:
         start_range = find_tomorrow_timestamp_range()
-    increase_by = 0
-    result = []
-    for _ in range(5):
-        result.append(list(map(lambda x, inc=increase_by: x + inc, start_range)))
-        increase_by += SECONDS_IN_A_DAY
-    return result
+    return [
+        [start_range[0] + i * SECONDS_IN_A_DAY, start_range[1] + i * SECONDS_IN_A_DAY]
+        for i in range(5)
+    ]
